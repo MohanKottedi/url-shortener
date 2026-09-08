58686ff547684cc51d35627c1fe2c5d6e5ba4865
MohanKottedi <marvokapello@gmail.com>
2026-09-08 23:57:47 +0530

    Project Started

---
+++ src/main/java/com/urlshortener/mohan/MohanApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortner.mohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohanApplication.class, args);
+	}
+
+}
